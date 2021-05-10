a48739595ae9fa646a8e6aecca9648840da5cba9
Greg Sonnier <gsonnier3@gmail.com>
2021-05-10 16:31:00 -0600

    buttons added for sorting employees table by first name or last name, seed added to api call for consistent fake employee list

--- src/components/Table.js
+++ src/components/Table.js
@@ -7,25 +7,58 @@ export default class Table extends Component {
 			people: [],
 			isLoaded: false,
 		};
+		this.firstSort = this.firstSort.bind(this);
+		this.lastSort = this.lastSort.bind(this);
 	}
 
 	componentDidMount() {
-		fetch("https://randomuser.me/api/?results=3")
+		fetch("https://randomuser.me/api/?results=10&seed=fakecompany")
 			.then((res) => res.json())
 			.then((result) => {
 				this.setState(() => ({
 					people: result.results,
 					isLoaded: true,
 				}));
-				console.log(this.state.people);
 			});
+	}
+
+	firstSort(e) {
+		e.preventDefault();
+
+		this.setState((state) => ({
+			people: state.people.sort((person1, person2) =>
+				person1.name.first > person2.name.first ? 1 : -1
+			),
+			isLoaded: true,
+		}));
+	}
+
+	lastSort(e) {
+		e.preventDefault();
+
+		this.setState((state) => ({
+			people: state.people.sort((person1, person2) =>
+				person1.name.last > person2.name.last ? 1 : -1
+			),
+			isLoaded: true,
+		}));
 	}
 
 	render() {
 		const { people, isLoaded } = this.state;
+
 		if (isLoaded) {
+			console.log(people);
 			return (
 				<div>
+					<div>
+						<button onClick={this.firstSort}>
+							Sort by first name
+						</button>
+						<button onClick={this.lastSort}>
+							Sort by last name
+						</button>
+					</div>
 					Employees:
 					<table>
 						<tr>
