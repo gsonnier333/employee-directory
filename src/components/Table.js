78d11de2a6e1aac43ab823e0b5bae0b6f127cc45
Greg Sonnier <gsonnier3@gmail.com>
2021-05-10 17:12:00 -0600

    new 'origList' property added to state, used in new 'unsort' button to revert to the original list/order

--- src/components/Table.js
+++ src/components/Table.js
@@ -6,18 +6,23 @@ export default class Table extends Component {
 		this.state = {
 			people: [],
 			isLoaded: false,
+			origList: [],
 		};
 		this.firstSort = this.firstSort.bind(this);
 		this.lastSort = this.lastSort.bind(this);
+		this.usFilter = this.usFilter.bind(this);
+		this.unsort = this.unsort.bind(this);
 	}
 
 	componentDidMount() {
-		fetch("https://randomuser.me/api/?results=10&seed=fakecompany")
+		fetch("https://randomuser.me/api/?results=30&seed=badcompany")
 			.then((res) => res.json())
 			.then((result) => {
+				let copy = Array.from(result.results); //copy the results so we can store them as two separate lists in our state instead of one list being pointed to by two different properties
 				this.setState(() => ({
 					people: result.results,
 					isLoaded: true,
+					origList: copy,
 				}));
 			});
 	}
@@ -25,22 +30,53 @@ export default class Table extends Component {
 	firstSort(e) {
 		e.preventDefault();
 
+		let sorted = this.state.people.sort((person1, person2) =>
+			person1.name.first > person2.name.first ? 1 : -1
+		);
+
 		this.setState((state) => ({
-			people: state.people.sort((person1, person2) =>
-				person1.name.first > person2.name.first ? 1 : -1
-			),
-			isLoaded: true,
+			people: sorted,
+			isLoaded: state.isLoaded,
+			origList: state.origList,
 		}));
 	}
 
 	lastSort(e) {
 		e.preventDefault();
 
+		let sorted = this.state.people.sort((person1, person2) =>
+			person1.name.last > person2.name.last ? 1 : -1
+		);
+
 		this.setState((state) => ({
-			people: state.people.sort((person1, person2) =>
-				person1.name.last > person2.name.last ? 1 : -1
-			),
-			isLoaded: true,
+			people: sorted,
+			isLoaded: state.isLoaded,
+			origList: state.origList,
+		}));
+	}
+
+	usFilter(e) {
+		e.preventDefault();
+
+		let filtered = this.state.people.filter(
+			(person) => person.location.country === "United States"
+		);
+		this.setState((state) => ({
+			people: filtered,
+			isLoaded: state.isLoaded,
+			origList: state.origList,
+		}));
+	}
+
+	unsort(e) {
+		e.preventDefault();
+
+		let unsorted = Array.from(this.state.origList); //copy original list so we don't accidentally point to it with state.people
+
+		this.setState((state) => ({
+			people: unsorted,
+			isLoaded: state.isLoaded,
+			origList: state.origList,
 		}));
 	}
 
@@ -58,6 +94,10 @@ export default class Table extends Component {
 						<button onClick={this.lastSort}>
 							Sort by last name
 						</button>
+						<button onClick={this.usFilter}>
+							Show only US employees
+						</button>
+						<button onClick={this.unsort}>Undo sort/filter</button>
 					</div>
 					Employees:
 					<table>
