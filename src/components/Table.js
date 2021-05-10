608f2cdd5ace9660a4b1e1255b0b3072715b4632
Greg Sonnier <gsonnier3@gmail.com>
2021-05-10 15:46:22 -0600

    Employee names and emails can be rendered to a table, minimal styling so far

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,20 +1,52 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export default class Table extends Component {
-    
-    componentDidMount() {
-        fetch("https://randomuser.me/api/")
-        .then(result => {
-            console.log(result);
-        })
-    }
-    
-    
-    render() {
-        return (
-            <div>
-                
-            </div>
-        )
-    }
+	constructor(props) {
+		super(props);
+		this.state = {
+			people: [],
+			isLoaded: false,
+		};
+	}
+
+	componentDidMount() {
+		fetch("https://randomuser.me/api/?results=3")
+			.then((res) => res.json())
+			.then((result) => {
+				this.setState(() => ({
+					people: result.results,
+					isLoaded: true,
+				}));
+				console.log(this.state.people);
+			});
+	}
+
+	render() {
+		const { people, isLoaded } = this.state;
+		if (isLoaded) {
+			return (
+				<div>
+					Employees:
+					<table>
+						<tr>
+							<th>Name</th>
+							<th>Email</th>
+						</tr>
+						{people.map((person) => {
+							return (
+								<tr>
+									<th>
+										{person.name.first} {person.name.last}
+									</th>
+									<th>{person.email}</th>
+								</tr>
+							);
+						})}
+					</table>
+				</div>
+			);
+		} else {
+			return <div>Name: Loading...</div>;
+		}
+	}
 }
